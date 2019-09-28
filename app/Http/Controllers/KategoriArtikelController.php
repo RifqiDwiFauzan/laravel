1b080e47457131f1a4a6e11c5aa805ccaab223a0
RifqiDwiFauzan <fauzanrdf123@gmail.com>
2019-09-28 08:40:00 +0800

    push

---
+++ app/Http/Controllers/KategoriArtikelController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\KategoriArtikel;
+
+class KategoriArtikelController extends Controller
+{
+    public function index(){
+        //Eloquent => ORM (Object Relational Mapping)
+        $listKategoriArtikel=KategoriArtikel::all(); //select * from kategori_artikel
+
+        //blade
+        return view('kategori_artikel.index', compact('listKategoriArtikel'));
+    }
+
+    public function show($id){
+        //Eloquent
+        //$kategoriArtikel=KategoriArtikel:where('id',$id)->first(); // select * from kategori_artikel where id=$id limit 1
+        $kategoriArtikel=KategoriArtikel::find($id);
+
+        return view('kategori_artikel.show', compact('kategoriArtikel'));
+    }
+}
